7cdc2982f7af46071fd67d50b9825d090e361d63
Aariyan Khan <your_email@gmail.com>
2026-02-13 15:11:01 +0530

    update CORS allowed origins to include only the relevant space-tech URL

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ const setCors = (req, res) => {
       : [
           "http://localhost:8080",
           "http://127.0.0.1:8080",
-          "https://spacesoftconsultancy.com",
-          "https://www.spacesoftconsultancy.com",
+          "https://space-tech-two.vercel.app",
+         
         ];
 
   const origin = req.headers.origin;
